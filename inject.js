30c29fc92127795d16809cfa73aa34d0cf5b322c
Yotomir <yotomir.karagiozov@gmail.com>
2025-12-26 12:46:43 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,10 +1,14 @@
 (function() {
     const checkBlockly = setInterval(() => {
+        // Проверяваме дали Scratch е заредил Blockly графичната среда
         if (window.Blockly && window.Blockly.setCatMode) {
             window.Blockly.setCatMode(true);
-            // Опресняваме работната площ, за да се появят ушите веднага
+            
+            // Форсираме прерисуване, за да се появят ушите веднага
             const workspace = window.Blockly.getMainWorkspace();
-            if (workspace) workspace.refreshRedraw();
+            if (workspace) {
+                workspace.refreshRedraw();
+            }
             
             clearInterval(checkBlockly);
             console.log("Cat Mode Activated!");
