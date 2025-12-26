84e2432a9164994df549539ed67e1c6904a7c26b
Yotomir <yotomir.karagiozov@gmail.com>
2025-12-26 12:47:15 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
 chrome.storage.local.get({ catModeEnabled: true }, (result) => {
-  if (result.catModeEnabled) {
-    const script = document.createElement('script');
-    script.src = chrome.runtime.getURL('inject.js');
-    script.onload = function() {
-        this.remove(); // Премахваме тага след зареждане, за да е чисто
-    };
-    (document.head || document.documentElement).appendChild(script);
-  }
+    if (result.catModeEnabled) {
+        const script = document.createElement('script');
+        // Използваме URL адреса на файла вътре в разширението
+        script.src = chrome.runtime.getURL('inject.js');
+        script.onload = function() {
+            this.remove();
+        };
+        (document.head || document.documentElement).appendChild(script);
+    }
 });
